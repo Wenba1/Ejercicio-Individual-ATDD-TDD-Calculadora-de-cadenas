f491cf199cad06696299a57e080f101839c9419a
Wendy Balderrama <balderramawendym@gmail.com>
2024-11-07 04:19:16 -0400

    Delimitadores especificos

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,12 +1,19 @@
 export function sumar(cadena) {
     if (!cadena) return 0;
 
-    const delimitadorRegex = /,|-/;
+    let delimitadorRegex = /,|-/;
+
+    const especificaionesDelimitador = cadena.match(/^\/\/\[(.*?)\]\n/);
+    if (especificaionesDelimitador) {
+        const delimitador = especificaionesDelimitador[1];
+        delimitadorRegex = new RegExp(`[${delimitador},-]`); 
+        cadena = cadena.slice(especificaionesDelimitador[0].length); 
+    }
 
     const numeros = cadena.split(delimitadorRegex);
     return numeros
-        .map(num => parseInt(num)) 
-        .reduce((acc, num) => acc + num, 0); 
+        .map(num => parseInt(num))
+        .reduce((acc, num) => acc + num, 0);
 }
-  
-  export default sumar;
+
+export default sumar;
--- src/calculadora.spec.js
+++ src/calculadora.spec.js
@@ -8,4 +8,12 @@ describe("Sumar", () => {
     it("Suma números separados por comas ahora incluimos guiones c:", () => {
         expect(sumar("1-2,3")).toEqual(6); 
     });
+
+    it("Maneja delimitador personalizado ';'", () => {
+        expect(sumar("//[;]\n6;7;4")).toEqual(17);
+    });
+
+    it("Maneja delimitador personalizado ';' junto con comas y guiones", () => {
+        expect(sumar("//[;]\n6,3-2;1")).toEqual(12);
+    });
 });
